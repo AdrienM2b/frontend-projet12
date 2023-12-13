7ff04ac352340cb81bd4b973079e2b8336bbf9d1
AdrienM2b <adrien.marchetti.6@gmail.com>
2023-12-13 17:48:22 +0100

    ajout du sass

---
+++ SportSee_app-Front/src/views/HorizontalNavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function HorizontalNavBar() {
+  return (
+    <div>
+      <p>Accueil</p>
+      <p>Accueil</p>
+      <p>Accueil</p>
+      <p>Accueil</p>
+    </div>
+  );
+}
