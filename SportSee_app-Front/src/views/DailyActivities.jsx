61a0104d65c5fd86d5bc0a4754c3e693e3fdf973
AdrienM2b <adrien.marchetti.6@gmail.com>
2023-12-14 10:23:30 +0100

    composant dailyactivity ok

--- SportSee_app-Front/src/views/DailyActivities.jsx
+++ SportSee_app-Front/src/views/DailyActivities.jsx
@@ -1,6 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { BarChart, XAxis, YAxis, Tooltip, Bar, Legend } from 'recharts';
+import {
+  BarChart,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Bar,
+  Legend,
+  CartesianGrid,
+  ReferenceLine,
+} from 'recharts';
 import axios from 'axios';
 
 const data = [
@@ -34,20 +43,45 @@ export default function DailyActivities() {
 
   const sessionsData = userData.data.sessions;
   const formatXAxis = (tickItem) => {
-    // Votre logique de formatage ici, par exemple :
-    return tickItem + 1;
+    return new Date(tickItem).getDate();
   };
 
   return (
     <div className='barchart_container'>
       <h3>Activité quotidienne</h3>
-      <BarChart width={900} height={300} data={sessionsData}>
-        <XAxis tickFormatter={formatXAxis} />
-        <YAxis dataKey='kilogram' />
+      <BarChart
+        width={900}
+        height={300}
+        data={sessionsData}
+        margin={{ top: 10, right: 30, left: 20, bottom: 50 }}
+      >
+        <CartesianGrid strokeDasharray='3 3' stroke='' />
+        <ReferenceLine yAxisId='right' y='40' />
+        <ReferenceLine yAxisId='right' y='80' />
+        <XAxis dataKey='day' stroke='#9B9EAC' tickFormatter={formatXAxis} />
+        <YAxis yAxisId='left' orientation='left' stroke='#9B9EAC' hide />
+        <YAxis yAxisId='right' orientation='right' stroke='#9B9EAC' />
         <Tooltip />
-        <Bar dataKey='calories' fill='#242424' barSize={20} />
-        <Bar dataKey='kilogram' fill='#e60000' barSize={20} />
-        <Legend align='right' verticalAlign='top' />
+        <Bar
+          yAxisId='right'
+          dataKey='kilogram'
+          fill='#242424'
+          barSize={10}
+          radius={[10, 10, 0, 0]}
+        />
+        <Bar
+          yAxisId='left'
+          dataKey='calories'
+          fill='#e60000'
+          barSize={10}
+          radius={[10, 10, 0, 0]}
+        />
+        <Legend
+          verticalAlign='top'
+          layout='horizontal'
+          align='right'
+          wrapperStyle={{ top: '-15%', right: '10%' }}
+        />
       </BarChart>
     </div>
   );
